f9e3bd5eec0252a5b943928c2b728e6e74da41e6
devataevsergej-droid <devataevsergej@gmail.com>
2026-05-14 02:48:31 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,6 +1,8 @@
 ﻿from .start import router as start_router
 from .menu import router as menu_router
+from .profile import router as profile_router
 
 def register_all_handlers(dp):
     dp.include_router(start_router)
     dp.include_router(menu_router)
+    dp.include_router(profile_router)
