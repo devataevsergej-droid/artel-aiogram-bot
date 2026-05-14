d219b0a4293e0336cb351c0f032ad66d7d4457df
devataevsergej-droid <devataevsergej@gmail.com>
2026-05-15 00:47:42 +0500

    Delete handlers directory

--- handlers/__init__.py
+++
@@ -1,6 +0,0 @@
-﻿from .start import router as start_router
-from .menu import router as menu_router
-
-def register_all_handlers(dp):
-    dp.include_router(start_router)
-    dp.include_router(menu_router)
